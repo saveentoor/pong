235f79bdd98fc6615ab7952f66f5cbfc8a0bd7b5
Saveen Toor <toorsaveen@gmail.com>
2019-02-07 15:24:22 -0800

    Add board class to my pong game

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,13 +1,29 @@
+import { SVG_NS } from "../settings";
+import Board from "./Board";
+
 export default class Game {
   constructor(element, width, height) {
     this.element = element;
     this.width = width;
     this.height = height;
 
-		// Other code goes here...
+    this.gameElement = document.getElementById(element);
+
+    this.board = new Board(this.width, this.height);
   }
 
   render() {
-		// More code goes here....
+    //empty out game elemment before re-rendering
+    this.gameElement.innerHTML = "";
+
+    let svg = document.createElementNS(SVG_NS, "svg");
+    svg.setAttributeNS(null, "width", this.width);
+    svg.setAttributeNS(null, "height", this.height);
+    svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
+    svg.setAttributeNS(null, "version", "1.1");
+    this.gameElement.appendChild(svg);
+
+    //here we will render all game elemtns inside the SVG
+    this.board.render(svg);
   }
 }
